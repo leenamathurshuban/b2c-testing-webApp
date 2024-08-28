efb25a3533b83c393c72d8a6dc06c1be5e41eb71
Leena mathur <leena.mathur@shubansoftware.com>
2024-08-28 18:36:33 +0530

    add space

--- src/components/Mac-sale/MacSaleHome.js
+++ src/components/Mac-sale/MacSaleHome.js
@@ -56,6 +56,7 @@ export default function MacSaleHome({ collections, loading, query }) {
               </Row>
 
             </div>
+            
           </Container>
         </div>
       </section>
