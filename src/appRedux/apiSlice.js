8cefd4099e842b2bd353406bbb739e89c3798732
Leena mathur <leena.mathur@shubansoftware.com>
2024-09-03 19:41:14 +0530

    add dynamically button and complete rediration

--- src/appRedux/apiSlice.js
+++ src/appRedux/apiSlice.js
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setChildCollectionData } from "./counterReducer";
 
 export const apiSlice = createApi({
   reducerPath: "apiProductSlice",
@@ -7,7 +8,22 @@ export const apiSlice = createApi({
   }),
   endpoints: (builder) => ({
     getCollections: builder.query({
-      query: (id) => `/collections/${id}`,
+      query: (id) => ({
+        url: `/collections/${id}`,
+        method: "GET",
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch, getState }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            setChildCollectionData({
+              data: result.data,
+            })
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      },
     }),
     getProductsByCollectionId: builder.query({
       query: ({ id, limit = 10, cursor = 1 }) =>
