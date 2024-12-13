5d6928ab109fb8fb11620cd6ad8d1049c85cbbfa
leenamathurshuban <78811868+leenamathurshuban@users.noreply.github.com>
2024-12-13 14:15:58 +0530

    fixing bugs1

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -73,10 +73,6 @@ export default function HomeComp() {
   }, [router.events]);
 
   console.log('order_data',router.query)
-  useEffect(()=>{
-    const base64 = window.atob(router.query)
-    console.log(base64)
-  },[router.isReady])
 
   return (
     <>
